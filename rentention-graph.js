eed5ed1f97d9a87a7f218243be5a0089a714ae20
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-05-22 18:10:12 +0530

    shadecolor input validation

--- rentention-graph.js
+++ rentention-graph.js
@@ -52,7 +52,13 @@ function getRows(data){
     return rows;
 }
 
-function shadeColor(color, percent) {
+function isValidHex(color){
+    return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color);
+}
+
+function shadeColor(color, percent) { //#
+    color = isValidHex(color) ? color : "#3f83a3"; //handling null color;
+    percent = 1.0 - Math.round((percent / 100) * 10 / 10);
     var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
     return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
 }
