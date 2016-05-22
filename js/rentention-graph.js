d6fb443157c4ce74f9f210fc7ae26d59afaefe0b
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-05-22 22:39:41 +0530

    beta version done

--- js/rentention-graph.js
+++ js/rentention-graph.js
@@ -43,7 +43,7 @@ function getRows(data){
             days = data[keys[key]];
             percentDays.push(keys[key]);
             for(var i = 0; i < days.length; i++){
-                percentDays.push(Math.round((days[i]/days[0] * 100) * 100) / 100);
+                percentDays.push(i > 0 ? Math.round((days[i]/days[0] * 100) * 100) / 100 : days[i]);
             }
             rows.push(percentDays);
             percentDays = [];
@@ -58,7 +58,7 @@ function isValidHex(color){
 
 function shadeColor(color, percent) { //#
     color = isValidHex(color) ? color : "#3f83a3"; //handling null color;
-    percent = 1.0 - Math.round((percent / 100) * 10 / 10);
+    percent = 1.0 - Math.ceil(percent / 10) / 10;
     var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
     return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
 }
@@ -72,16 +72,17 @@ var options = {
         "22-05-2016" : [200, 10, 20, 30, 40, 10, 20, 20],
         "23-05-2016" : [300, 200, 150, 50, 20, 20, 90, 100 ],
         "24-05-2016" : [200, 110, 150, 50, 10, 20, 30, 40],
-        "25-05-2016" : [100, 10, 10, 50, 20, 20, 60, 10]
+        "25-05-2016" : [100, 10, 10, 50, 20, 20, 60, 0],
     },
     startDate : "",
     endDate : "",
+    dateFormat : "",
     title : "Retention Analysis"
 };
 
 $.fn.Retention = function (options) {
+
     var graphTitle = options.title || "Retention Graph";
-    console.log(graphTitle);
     var data = options.data || null;
 
     var container = d3.select(this[0]).append("div")
@@ -125,12 +126,14 @@ $.fn.Retention = function (options) {
         .attr("class", "box-body");
 
     var table = body.append("table")
-        .attr("class", "table table-bordered");
+        .attr("class", "table table-bordered text-center");
 
-    var headData = ["Date\\Days", "day0", "day1", "day2", "day3", "day4", "day5", "day6", "day7"];
+    var headData = ["Date \\ Days", "day0", "day1", "day2", "day3", "day4", "day5", "day6", "day7"];
 
     var tHead = table.append("thead")
-        .append("tr").selectAll("td")
+        .append("tr")
+        .attr("class", "retention-thead")
+        .selectAll("td")
         .data(headData)
         .enter()
         .append("td")
@@ -163,7 +166,8 @@ $.fn.Retention = function (options) {
             else
                 return "days";
         })
-        .attr("style", function (d) {
+        .attr("style", function (d, i) {
+            if(i > 1)
             return "background-color :" + shadeColor("", d);
         })
         .append("div")
