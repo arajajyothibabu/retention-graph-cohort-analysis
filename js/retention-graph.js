4915851314bacca7e8205d68b99cdbdc307a1dbe
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-05-26 22:46:18 +0530

    prototypic functions

--- js/retention-graph.js
+++ js/retention-graph.js
@@ -22,9 +22,12 @@
 
         function Retention(element, settings) {
 
-            var _ = this, dataSettings;
+            var _this = this;
 
-            _.defaults = {
+            _this.element = element;
+            _this.originalOptions = settings;
+
+            _this.defaults = {
                 data : {},
                 startDate : null,
                 endDate : null,
@@ -35,29 +38,11 @@
                 }
             };
 
-            _.initials = {
-                //TODO:
-            };
+            _this.options = this.generateOptions();
 
-            $.extend(_, _.initials);
+            _this.proceedFlag = Object.keys(_this.options.data).length;
 
-            dataSettings = $(element).data('retention') || {};
-
-            _.options = $.extend({}, _.defaults, dataSettings, settings);
-
-            _.originalSettings = _.options;
-
-            if (typeof document.mozHidden !== 'undefined') {
-                _.hidden = 'mozHidden';
-                _.visibilityChange = 'mozvisibilitychange';
-            } else if (typeof document.webkitHidden !== 'undefined') {
-                _.hidden = 'webkitHidden';
-                _.visibilityChange = 'webkitvisibilitychange';
-            }
-
-            _.instanceId = instanceId++;
-
-            _.init(true);
+            _this.init();
 
         }
 
@@ -65,21 +50,106 @@
 
     }());
 
-    $.fn.retention = function() {
-        var _this = this,
-            options = arguments[0],
-            args = Array.prototype.slice.call(arguments, 1),
-            length = _this.length,
-            i = 0,
-            ret;
-        for (i; i < length; i++) {
-            if (typeof options == 'object' || typeof options == 'undefined')
-                _this[i].retention = new Retention(_[i], options);
-            else
-                ret = _this[i].retention[options].apply(_this[i].retention, args);
-            if (typeof ret != 'undefined') return ret;
+    Retention.prototype.init = function () {
+        if(this.proceedFlag){
+            //TODO
+        }else{
+            //TODO
         }
-        return _;
+    };
+
+    Retention.prototype.generateOptions = function () {
+        var options = {};
+        var keys = Object.keys(this.defaults);
+        var key;
+        for(var i in keys){
+            key = keys[i];
+            options[key] = typeof this.originalOptions[key] == 'undefined' ? this.defaults[key] : this.originalOptions[key];
+        }
+        return options;
+    };
+
+    Retention.prototype.getRows = function(){
+        var rows = [];
+        var keys = Object.keys(this.options.data);
+        for(var key in keys){
+            if(data.hasOwnProperty(keys[key])) {
+                rows.push([keys[key]].concat(data[keys[key]]));
+            }
+        }
+        return rows;
+    };
+
+    Retention.prototype.formatDate = function(date){
+        var formattedDate = (this.options.dateDisplayFormat != null) ? (this.options.dateFormat ? moment(date, this.options.dateFormat).format(this.options.dateDisplayFormat) : moment(date).format(this.options.dateDisplayFormat)) : date;
+        return formattedDate;
+    };
+
+    Retention.prototype.getPercentage = function (total, value) {
+        return Math.round((value/total * 100) * 100) / 100;
+    };
+
+    Retention.prototype.isValidHex = function(color){
+        return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color);
+    };
+
+    Retention.prototype.shadeColor = function(color, percent) { //#
+        color = this.isValidHex(color) ? color : "#3f83a3"; //handling null color;
+        percent = 1.0 - Math.ceil(percent / 10) / 10;
+        var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+        return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+    };
+
+    Retention.prototype.tooltipData = function(date, total, count, dayIndex){
+        return  "Of " + total + " users came on " + moment(date).format("MMM DD") + ", " + (count + " were active on " + moment(date).add(dayIndex-1, "days").format("MMM DD"));
+    };
+
+    Retention.prototype.getHeaderData = function(){
+        var headerDataAppender = $("input[name='retention-switch']:checked").val(); //changes by selection of switch
+        var headerData = [];
+        for(var i = 0; i < 9; i++){
+            headerData.push(i > 0? (headerDataAppender + (i-1)) : ("Date \\ " + headerDataAppender + "s"));
+        }
+        return headerData;
+    };
+
+    Retention.prototype.generateRow = function(data){
+        var _this = this;
+        var row = $('<tr />');
+        var date = data[0];
+        var count = data[1] || 1; //to handle divisionBy0
+        var td, div;
+        for(var key in data){
+            var className = key > 0 ? "retention-cell" : "retention-date";
+            td = $('<td />', {
+                class : className,
+                style : function () {
+                    if(key > 1)
+                        return "background-color :" + _this.shadeColor("", data[key]);
+                }
+            }).click(function (){
+                _this.cellClickEvent != null ? _this.cellClickEvent(date, 1) : null;
+            }).appendTo(row);
+            div = $('<div />', {
+                'data-toggle' : "tooltip",
+                title : function () {
+                    if(key > 1 && data[key] != 0)
+                        return  _this.tooltipData(date, count, data[key], key);
+                },
+                text : function () {
+                    return key > 1 ? (_this.getPercentage(count, data[key]) + "%" ) : data[key];
+                }
+            }).appendTo(td);
+        }
+        return row;
+    };
+
+    $.fn.retention = function(options) {
+        if(typeof options == 'object'){
+            new Retention(this, options);
+        }else{
+            throw new Error("Need data to show retention graph..!");
+        }
     };
 
 }));
@@ -106,10 +176,6 @@ function getRows(data, dateFormat, dateDisplayFormat){
         }
     }
     return rows;
-}
-
-function isValidHex(color){
-    return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color);
 }
 
 function shadeColor(color, percent) { //#
