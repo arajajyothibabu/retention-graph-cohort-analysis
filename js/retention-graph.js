fcf34dd0e637ecec9fba7a0d382a59ff6aeb57c8
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-05-29 21:52:05 +0530

    standard version ready with modular object functions

--- js/retention-graph.js
+++ js/retention-graph.js
@@ -18,8 +18,6 @@
 
     Retention = (function() {
 
-        var instanceId = 0;
-
         function Retention(element, settings) {
 
             var _this = this;
@@ -29,13 +27,15 @@
 
             _this.defaults = {
                 data : {},
+                title : "Retention Analysis",
                 startDate : null,
                 endDate : null,
                 inputDateFormat : null,
                 dateDisplayFormat: null,
                 cellClickEvent : function (d, i) {
                     //do something
-                }
+                },
+                showEmptyDataMessage : false
             };
 
             _this.options = this.generateOptions();
@@ -51,17 +51,51 @@
     }());
 
     Retention.prototype.init = function () {
+
+        var container = this.getContainer();
+
+        $("input[value='day']").attr("checked", ""); //default days checked
+
+        var body = this.getBody();
+        body.appendTo(container);
+
         if(this.proceedFlag){
-            //TODO
+            this.start(body);
         }else{
-            //TODO
+            this.end(body);
         }
+    };
+
+    Retention.prototype.end = function (body) {
+        if(this.options.showEmptyDataMessage){
+            $('<h3 />', {
+                align : 'center',
+                style : 'color:red'
+            }).appendTo(body);
+        }
+    };
+
+    Retention.prototype.start = function (body) {
+        $(body).html('');
+        var table = this.getTable();
+        table.appendTo(body);
+        var tableHeader = this.generateTableHeader(table);
+        tableHeader.appendTo(table);
+
+        var tbody = $('<tbody />').appendTo(table);
+
+        var rowsData = this.getRows();
+
+        for(var row in rowsData){
+            this.generateRow(rowsData[row]).appendTo(tbody);
+        }
+
     };
 
     Retention.prototype.getContainer = function () {
         var container = $('<div />', {
             class : 'box'
-        }).appendTo(this);
+        }).appendTo(this.element);
 
         var header = $('<div />', {
             class : "box-header with-border"
@@ -69,7 +103,7 @@
 
         var title = $('<p />', {
             class : "box-title",
-            text : graphTitle
+            text : this.options.title
         }).appendTo(header);
 
         var controls = $('<div />', {
@@ -116,6 +150,22 @@
         return table;
     };
 
+    Retention.prototype.generateTableHeader = function (table) {
+        var tHead = $('<thead />').appendTo(table);
+        var tHeadRow = $('<tr />', {
+            class : "retention-thead"
+        }).appendTo(tHead);
+
+        var headerData = this.getHeaderData();
+        for(var key in headerData){
+            $('<td />', {
+                class : "retention-cell",
+                text : headerData[key]
+            }).appendTo(tHeadRow);
+        }
+        return tHead;
+    };
+
     Retention.prototype.generateOptions = function () {
         var options = {};
         var keys = Object.keys(this.defaults);
@@ -131,16 +181,15 @@
         var rows = [];
         var keys = Object.keys(this.options.data);
         for(var key in keys){
-            if(data.hasOwnProperty(keys[key])) {
-                rows.push([keys[key]].concat(data[keys[key]]));
+            if(this.options.data.hasOwnProperty(keys[key])) {
+                rows.push([keys[key]].concat(this.options.data[keys[key]]));
             }
         }
         return rows;
     };
 
     Retention.prototype.formatDate = function(date){
-        var formattedDate = (this.options.dateDisplayFormat != null) ? (this.options.dateFormat ? moment(date, this.options.dateFormat).format(this.options.dateDisplayFormat) : moment(date).format(this.options.dateDisplayFormat)) : date;
-        return formattedDate;
+        return (this.options.dateDisplayFormat != null) ? (this.options.dateFormat ? moment(date, this.options.dateFormat).format(this.options.dateDisplayFormat) : moment(date).format(this.options.dateDisplayFormat)) : date;
     };
 
     Retention.prototype.getPercentage = function (total, value) {
@@ -183,7 +232,7 @@
                 class : className,
                 style : function () {
                     if(key > 1)
-                        return "background-color :" + _this.shadeColor("", data[key]);
+                        return "background-color :" + _this.shadeColor("", _this.getPercentage(count, data[key]));
                 },
                 date : date,
                 day : key-1
@@ -210,84 +259,9 @@
         }
     };
 
+    //some dom queries
+
 }));
-
-
-
-function getRows(data, dateFormat, dateDisplayFormat){
-    var rows = [];
-    var keys = Object.keys(data);
-    var days = [];
-    var percentDays = [];
-    var date;
-    for(var key in keys){
-        if(data.hasOwnProperty(keys[key])) {
-            days = data[keys[key]];
-            date = (dateDisplayFormat != null) ? (dateFormat ? moment(keys[key], dateFormat).format(dateDisplayFormat) : moment(keys[key]).format(dateDisplayFormat)) : keys[key];
-            //console.log(moment(keys[key], dateFormat).format(dateDisplayFormat), moment(keys[key]).format(dateDisplayFormat));
-            percentDays.push(date);
-            for(var i = 0; i < days.length; i++){
-                percentDays.push(i > 0 ? Math.round((days[i]/days[0] * 100) * 100) / 100 : days[i]);
-            }
-            rows.push(percentDays);
-            percentDays = [];
-        }
-    }
-    return rows;
-}
-
-function shadeColor(color, percent) { //#
-    color = isValidHex(color) ? color : "#3f83a3"; //handling null color;
-    percent = 1.0 - Math.ceil(percent / 10) / 10;
-    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
-    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
-}
-
-function tooltipData(date, total, count, dayIndex){
-    return  "Of " + total + " users came on " + moment(date).format("MMM DD") + ", " + (count + "% were active on " + moment(date).add(dayIndex-1, "days").format("MMM DD"));
-}
-
-function getHeaderData(){
-    var headerDataAppender = $("input[name='retention-switch']:checked").val(); //changes by selection of switch
-    var headerData = [];
-    for(var i = 0; i < 9; i++){
-        if(i == 0)
-            headerData.push("Date \\ " + headerDataAppender + "s");
-        else
-            headerData.push(headerDataAppender + (i-1));
-    }
-    return headerData;
-}
-
-function generateRow(data, cellClickEvent){
-    var row = $('<tr />');
-    var date = data[0];
-    var count = data[1] || 1;
-    var td, div;
-    for(var key in data){
-        var className = key > 0 ? "retention-cell" : "retention-date";
-        td = $('<td />', {
-            class : className,
-            style : function () {
-                if(key > 1)
-                    return "cursor: pointer; background-color :" + shadeColor("", data[key]);
-            },
-        }).click(function (){
-            cellClickEvent != null ? cellClickEvent(date, 1) : null
-        }).appendTo(row);
-        div = $('<div />', {
-            'data-toggle' : "tooltip",
-            title : function () {
-                if(key > 1 && data[key] != 0)
-                    return  tooltipData(date, count, data[key], key);
-            },
-            text : function () {
-                return data[key] + (key > 1 ? "%" : "");
-            }
-        }).appendTo(td);
-    }
-    return row;
-}
 
 var options = {
     data : {
@@ -304,90 +278,4 @@ var options = {
     cellClickEvent : function(date, day){
         window.location = "https://www.google.com?date=" + date + "$day="+ day;
     }
-};
-
-$.fn.Retention = function (options) {
-
-    var graphTitle = options.title || "Retention Graph";
-    var data = options.data || null;
-    var dateFormat = options.dateFormat || null;
-    var dateDisplayFormat = options.dateDisplayFormat || null;
-    var cellClickEvent = typeof options.cellClickEvent == 'function' ? options.cellClickEvent : null;
-    if(data == null || Object.keys(data).length == 0)
-        return;
-
-    var container = $('<div />', {
-        class : 'box'
-    }).appendTo(this);
-
-    var header = $('<div />', {
-        class : "box-header with-border"
-    }).appendTo(container);
-
-    var title = $('<p />', {
-        class : "box-title",
-        text : graphTitle
-    }).appendTo(header);
-
-    var controls = $('<div />', {
-        class : "box-tools"
-    }).appendTo(header);
-
-    var dateRange = $('<input />',{
-        id : "date-range",
-        type : "hidden"
-    }).appendTo(controls); //TODO: implement daterangepicker
-
-    var switchContainer = $('<div />', {
-        class : "switch-field"
-    }).appendTo(controls);
-
-    var switchData = ["day", "week", "month"];
-
-    for(var key in switchData){
-        $('<input />', {
-            type : "radio",
-            name : "retention-switch",
-            id : switchData[key],
-            value : switchData[key]
-        }).appendTo(switchContainer);
-        $('<label />', {
-            for : switchData[key],
-            text : switchData[key] + "s" //appending s for "days"
-        }).appendTo(switchContainer);
-    }
-
-    $("input[value='day']").attr("checked", ""); //default days checked
-
-    var body = $('<div />', {
-        class : "box-body"
-    }).appendTo(container);
-
-    var table = $('<table />', {
-        class : "table table-bordered text-center"
-    }).appendTo(body);
-
-    var thead = $('<thead />').appendTo(table);
-    var tHeadRow = $('<tr />', {
-        class : "retention-thead"
-    }).appendTo(thead);
-
-    var headerData = getHeaderData();
-    for(var key in headerData){
-        $('<td />', {
-            class : "retention-cell",
-            text : headerData[key]
-        }).appendTo(tHeadRow);
-    }
-
-    var tbody = $('<tbody />').appendTo(table);
-
-    var rowsData = getRows(data, dateFormat, dateDisplayFormat);
-
-    for(var row in rowsData){
-        generateRow(rowsData[row], cellClickEvent).appendTo(tbody);
-    }
-
-    $('[data-toggle="tooltip"]').tooltip(); //calling bootstrap tooltip
-
 };
